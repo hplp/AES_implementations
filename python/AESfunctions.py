c21c2f5ec3f3bc4cd7f38eb4d1f43959889c9ec0
Sergiu Mosanu <sm7ed@virginia.edu>
2017-10-17 15:01:55 -0400

    added AES_128 python files

---
+++ python/AESfunctions.py
@@ -0,0 +1,193 @@
+from AEStables import *
+
+def KeyExpansion(inputKey):
+	def KeyExpansionCore(in4, i):
+		# RotWord rotates left
+		t = in4[0]
+		in4[0] = in4[1]
+		in4[1] = in4[2]
+		in4[2] = in4[3]
+		in4[3] = t
+		# SubWord substitutes with S-Box value
+		in4[0] = s_box[in4[0]]
+		in4[1] = s_box[in4[1]]
+		in4[2] = s_box[in4[2]]
+		in4[3] = s_box[in4[3]]
+		# RCon (round constant) 1st byte XOR rcon
+		in4[0] = in4[0] ^ rcon[i]
+		# return KeyExpansionCore
+		return in4
+
+	# declare expandedKeys and copy the inputKey at its beginning
+	expandedKeys=[0]*176
+	for i in range(16): expandedKeys[i] = inputKey[i]
+
+	# Variables
+	bytesGenerated = 16 # already generated 16 bytes
+	rconIteration = 1
+	temp=[0]*4
+
+	while(bytesGenerated < 176):
+		# Read previously generated last 4 bytes
+		for i in range(4):
+			temp[i] = expandedKeys[i + bytesGenerated - 4]
+
+		# Perform the core once for each 16 byte key
+		if(bytesGenerated % 16 == 0):
+			temp = KeyExpansionCore(temp, rconIteration)
+			rconIteration = rconIteration + 1
+
+		# XOR temp with [bytesGenerated-16] and store in expandedKeys
+		for i in range(4):
+			expandedKeys[bytesGenerated] = expandedKeys[bytesGenerated - 16] ^ temp[i]
+			bytesGenerated = bytesGenerated + 1
+	#print(len(expandedKeys),expandedKeys)
+	return expandedKeys
+
+def SubBytes(state):
+	for i in range(16): state[i] = s_box[state[i]]
+	return state
+
+def InvSubBytes(state):
+	for i in range(16): state[i] = rs_box[state[i]]
+	return state
+
+def ShiftRows(state):
+	tmp_state=[0]*16
+	tmp_state[0] = state[0]
+	tmp_state[1] = state[5]
+	tmp_state[2] = state[10]
+	tmp_state[3] = state[15]
+
+	tmp_state[4] = state[4]
+	tmp_state[5] = state[9]
+	tmp_state[6] = state[14]
+	tmp_state[7] = state[3]
+
+	tmp_state[8] = state[8]
+	tmp_state[9] = state[13]
+	tmp_state[10] = state[2]
+	tmp_state[11] = state[7]
+
+	tmp_state[12] = state[12]
+	tmp_state[13] = state[1]
+	tmp_state[14] = state[6]
+	tmp_state[15] = state[11]
+	return tmp_state
+
+def InvShiftRows(state):
+	tmp_state=[0]*16
+	tmp_state[0] = state[0]
+	tmp_state[1] = state[13]
+	tmp_state[2] = state[10]
+	tmp_state[3] = state[7]
+
+	tmp_state[4] = state[4]
+	tmp_state[5] = state[1]
+	tmp_state[6] = state[14]
+	tmp_state[7] = state[11]
+
+	tmp_state[8] = state[8]
+	tmp_state[9] = state[5]
+	tmp_state[10] = state[2]
+	tmp_state[11] = state[15]
+
+	tmp_state[12] = state[12]
+	tmp_state[13] = state[9]
+	tmp_state[14] = state[6]
+	tmp_state[15] = state[3]
+	return tmp_state
+
+def MixColumns(state):
+	tmp_state=[0]*16
+	tmp_state[0] = mul02[state[0]] ^ mul03[state[1]] ^ state[2] ^ state[3]
+	tmp_state[1] = state[0] ^ mul02[state[1]] ^ mul03[state[2]] ^ state[3]
+	tmp_state[2] = state[0] ^ state[1] ^ mul02[state[2]] ^ mul03[state[3]]
+	tmp_state[3] = mul03[state[0]] ^ state[1] ^ state[2] ^ mul02[state[3]]
+
+	tmp_state[4] = mul02[state[4]] ^ mul03[state[5]] ^ state[6] ^ state[7]
+	tmp_state[5] = state[4] ^ mul02[state[5]] ^ mul03[state[6]] ^ state[7]
+	tmp_state[6] = state[4] ^ state[5] ^ mul02[state[6]] ^ mul03[state[7]]
+	tmp_state[7] = mul03[state[4]] ^ state[5] ^ state[6] ^ mul02[state[7]]
+
+	tmp_state[8] = mul02[state[8]] ^ mul03[state[9]] ^ state[10] ^ state[11]
+	tmp_state[9] = state[8] ^ mul02[state[9]] ^ mul03[state[10]] ^ state[11]
+	tmp_state[10] = state[8] ^ state[9] ^ mul02[state[10]] ^ mul03[state[11]]
+	tmp_state[11] = mul03[state[8]] ^ state[9] ^ state[10] ^ mul02[state[11]]
+
+	tmp_state[12] = mul02[state[12]] ^ mul03[state[13]] ^ state[14] ^ state[15]
+	tmp_state[13] = state[12] ^ mul02[state[13]] ^ mul03[state[14]] ^ state[15]
+	tmp_state[14] = state[12] ^ state[13] ^ mul02[state[14]] ^ mul03[state[15]]
+	tmp_state[15] = mul03[state[12]] ^ state[13] ^ state[14] ^ mul02[state[15]]
+	return tmp_state
+
+def InvMixColumns(state):
+	tmp_state=[0]*16
+	tmp_state[0]  = mul14[state[0]] ^ mul11[state[1]] ^ mul13[state[2]] ^ mul09[state[3]]
+	tmp_state[1]  = mul09[state[0]] ^ mul14[state[1]] ^ mul11[state[2]] ^ mul13[state[3]]
+	tmp_state[2]  = mul13[state[0]] ^ mul09[state[1]] ^ mul14[state[2]] ^ mul11[state[3]]
+	tmp_state[3]  = mul11[state[0]] ^ mul13[state[1]] ^ mul09[state[2]] ^ mul14[state[3]]
+
+	tmp_state[4]  = mul14[state[4]] ^ mul11[state[5]] ^ mul13[state[6]] ^ mul09[state[7]]
+	tmp_state[5]  = mul09[state[4]] ^ mul14[state[5]] ^ mul11[state[6]] ^ mul13[state[7]]
+	tmp_state[6]  = mul13[state[4]] ^ mul09[state[5]] ^ mul14[state[6]] ^ mul11[state[7]]
+	tmp_state[7]  = mul11[state[4]] ^ mul13[state[5]] ^ mul09[state[6]] ^ mul14[state[7]]
+
+	tmp_state[8]  = mul14[state[8]] ^ mul11[state[9]] ^ mul13[state[10]] ^ mul09[state[11]]
+	tmp_state[9]  = mul09[state[8]] ^ mul14[state[9]] ^ mul11[state[10]] ^ mul13[state[11]]
+	tmp_state[10] = mul13[state[8]] ^ mul09[state[9]] ^ mul14[state[10]] ^ mul11[state[11]]
+	tmp_state[11] = mul11[state[8]] ^ mul13[state[9]] ^ mul09[state[10]] ^ mul14[state[11]]
+
+	tmp_state[12] = mul14[state[12]] ^ mul11[state[13]] ^ mul13[state[14]] ^ mul09[state[15]]
+	tmp_state[13] = mul09[state[12]] ^ mul14[state[13]] ^ mul11[state[14]] ^ mul13[state[15]]
+	tmp_state[14] = mul13[state[12]] ^ mul09[state[13]] ^ mul14[state[14]] ^ mul11[state[15]]
+	tmp_state[15] = mul11[state[12]] ^ mul13[state[13]] ^ mul09[state[14]] ^ mul14[state[15]]
+	return tmp_state
+
+def AddRoundKey(state, roundKey):
+	for i in range(16): state[i] = state[i] ^ roundKey[i]
+	return state
+
+def AES_Encrypt(message, expandedKey):
+	state=[0]*16
+	# Convert chars to HEX (INT) using ASCII
+	for i in range(16): state[i] = ord(message[i])
+
+	# Whitening
+	state = AddRoundKey(state, expandedKey[0:16])
+
+	# Rounds
+	numberOfRounds = 9
+	for i in range(numberOfRounds):
+		state = SubBytes(state)
+		state = ShiftRows(state)
+		state = MixColumns(state)
+		state = AddRoundKey(state, expandedKey[16*(i+1):16*(i+1)+16])
+
+	# Final Round
+	state = SubBytes(state)
+	state = ShiftRows(state)
+	state = AddRoundKey(state, expandedKey[160:176])
+
+	return state
+
+def AES_Decrypt(encrypted_message, expandedKey):
+	state=encrypted_message
+
+	# Whitening
+	state = AddRoundKey(state, expandedKey[160:176])
+
+	# Rounds
+	numberOfRounds = 9
+	for i in range(numberOfRounds):
+		state = InvShiftRows(state)
+		state = InvSubBytes(state)
+		state = AddRoundKey(state, expandedKey[16*(numberOfRounds-i):16*(numberOfRounds+1-i)])
+		state = InvMixColumns(state)
+		
+	# Final Round
+	state = InvShiftRows(state)
+	state = InvSubBytes(state)
+	state = AddRoundKey(state, expandedKey[0:16])
+
+	return state
---
+++ python/script.py
@@ -0,0 +1,59 @@
+from AESfunctions import *
+
+# test input data
+message = "The quick brown fox jumps over the lazy dog."
+
+# AES can use 16, 24, or 32 byte keys (128, 192, and 256 bits respectively)
+# test key lenghth and value
+key_lenghth = 16
+if key_lenghth == 16:
+	key = [0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf]
+elif key_lenghth == 24:
+	key = [0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17]
+elif key_lenghth == 32:
+	key = [0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f]
+
+print("message: ",len(message),message)
+expandedKey=KeyExpansion(key)
+print("expandedKey: ",len(expandedKey),expandedKey)
+
+# pad message with spaces to be integer chunks of 16 bytes
+if len(message)%16:
+	message = message.ljust(len(message) + 16 - len(message)%16, chr(0))
+print("Original: ",len(message),message)
+
+message_list=[0]*len(message)
+# Convert chars to INT using ASCII
+for i in range(len(message)): message_list[i] = ord(message[i])
+print("ASCII vals: ",len(message_list),message_list)
+
+###
+encrypted_message=[0]*len(message)
+index = 0
+while(message):
+	encrypted_message[index:index+16] = AES_Encrypt(message[:16], expandedKey)
+	message = message[16:]
+	index = index + 16
+
+print("Encrypted: ",len(encrypted_message),encrypted_message)
+
+encrypted_message_hex=[0]*len(encrypted_message)
+index = 0
+for v in encrypted_message:
+	encrypted_message_hex[index] = hex(v)
+	index = index + 1
+
+print("Encr HEX: ",len(encrypted_message_hex),encrypted_message_hex)
+
+###
+decrypted_message=[0]*len(encrypted_message)
+index = 0
+while(encrypted_message):
+	decrypted_message[index:index+16] = AES_Decrypt(encrypted_message[:16], expandedKey)
+	encrypted_message = encrypted_message[16:]
+	index = index + 16
+
+print("Decrypted: ",len(decrypted_message),decrypted_message)
+
+rec_message = "".join(chr(i) for i in decrypted_message)
+print("Decrypted chars: ",len(rec_message),rec_message)
