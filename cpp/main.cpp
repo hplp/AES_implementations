be6e7365b3ff2624974cebbb9d9b0e1e5c782487
Sergiu Mosanu <sm7ed@virginia.edu>
2017-10-18 14:59:32 -0400

    updated repo structure

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {
 	int Nb = 4;
-	int Nk = 4; //6 8
-	int CipherKeyLenghth = Nk
+	int Nk = 4; //6 8 [32-bit words] in cipher key
+	int CipherKeyLenghth = Nk * 4; // bytes
 	cout << "AES tbd" << endl;
     return 0;
 }
