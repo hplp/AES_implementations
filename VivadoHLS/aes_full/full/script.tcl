fa4754aea7f86790cf92dc077d38250f57f0c990
Sergiu Mosanu <sm7ed@virginia.edu>
2018-11-11 20:59:59 -0500

    add report for HLS AES Full

--- VivadoHLS/aes_full/full/script.tcl
+++ VivadoHLS/aes_full/full/script.tcl
@@ -1,7 +1,7 @@
 ############################################################
 ## This file is generated automatically by Vivado HLS.
 ## Please DO NOT edit it.
-## Copyright (C) 1986-2017 Xilinx, Inc. All Rights Reserved.
+## Copyright (C) 1986-2018 Xilinx, Inc. All Rights Reserved.
 ############################################################
 open_project aes_full
 set_top AES_Full
@@ -11,7 +11,7 @@ add_files source/AEStables.h
 add_files -tb source/test_AES_full.cpp
 open_solution "full"
 set_part {xc7z020clg400-1}
-create_clock -period 10 -name default
+create_clock -period 6.67 -name default
 set_clock_uncertainty 0
 #source "./aes_full/full/directives.tcl"
 csim_design -clean
