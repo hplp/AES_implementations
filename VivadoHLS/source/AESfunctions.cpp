dad420d77202e41dfd991438e98432be1bd09197
Sergiu Mosanu <sm7ed@virginia.edu>
2019-02-12 11:35:37 -0500

    correct, optimize and improve all code

--- VivadoHLS/source/AESfunctions.cpp
+++ VivadoHLS/source/AESfunctions.cpp
@@ -1,5 +1,64 @@
+// https://en.wikipedia.org/wiki/Advanced_Encryption_Standard
+
 #include "AESfunctions.h"
 #include "AEStables.h"
+
+void KeyExpansionCore(unsigned char* in4, unsigned char i)
+{
+    // RotWord rotates left
+    // SubWord substitutes with S - Box value
+    unsigned char t = in4[0];
+    in4[0] = s_box[in4[1]];
+    in4[1] = s_box[in4[2]];
+    in4[2] = s_box[in4[3]];
+    in4[3] = s_box[t];
+    // RCon (round constant) 1st byte XOR rcon
+    in4[0] = in4[0] ^ rcon[i];
+}
+
+void SubWord(unsigned char* in4)
+{
+    // SubWord substitutes with S - Box value
+    in4[0] = s_box[in4[0]];
+    in4[1] = s_box[in4[1]];
+    in4[2] = s_box[in4[2]];
+    in4[3] = s_box[in4[3]];
+}
+
+void KeyExpansion(unsigned char* inputKey, unsigned short Nk, unsigned char* expandedKey)
+{
+    unsigned short Nr = (Nk > Nb) ? Nk + 6 : Nb + 6; // = 10, 12 or 14 rounds
+    // Copy the inputKey at the beginning of expandedKey
+    for (unsigned short i = 0; i < Nk * rows; i++) { expandedKey[i] = inputKey[i]; }
+
+    // Variables
+    unsigned short byGen = Nk * rows;
+    unsigned short rconIdx = 1;
+    unsigned char temp[rows];
+
+    // Generate expanded keys
+    while (byGen < (Nr + 1) * stt_lng)
+    {
+        // Read previously generated last 4 bytes (last word)
+        for (unsigned short i = 0; i < rows; i++) { temp[i] = expandedKey[byGen - rows + i]; }
+        // Perform KeyExpansionCore once for each 16 byte key
+        if (byGen % (Nk * rows) == 0)
+        {
+            KeyExpansionCore(temp, rconIdx);
+            rconIdx++;
+        }
+        else if ((Nk > 6) && (byGen % (Nk * rows) == (4 * rows)))
+        {
+            SubWord(temp);
+        }
+        // XOR temp with [bytesGenerated-16] and store in expandedKeys
+        for (unsigned short i = 0; i < rows; i++) {
+            expandedKey[byGen] = expandedKey[byGen - Nk * rows] ^ temp[i];
+            byGen++;
+        }
+    }
+}
+
 
 void SubBytes(unsigned char* state) {
 #pragma HLS inline off
@@ -16,6 +75,7 @@ void InvSubBytes(unsigned char* state) {
 		state[i] = inverted_s_box[state[i]];
 	}
 }
+
 
 void ShiftRows(unsigned char* state) {
 #pragma HLS inline off
@@ -40,6 +100,7 @@ void ShiftRows(unsigned char* state) {
 	tmp_state[14] = state[6];
 	tmp_state[15] = state[11];
 	for (unsigned short i = 0; i < stt_lng; i++) {
+#pragma HLS unroll
 		state[i] = tmp_state[i];
 	}
 }
@@ -67,9 +128,11 @@ void InvShiftRows(unsigned char* state) {
 	tmp_state[14] = state[6];
 	tmp_state[15] = state[3];
 	for (unsigned short i = 0; i < stt_lng; i++) {
+#pragma HLS unroll
 		state[i] = tmp_state[i];
 	}
 }
+
 
 void MixColumns(unsigned char* state) {
 #pragma HLS inline off
@@ -94,6 +157,7 @@ void MixColumns(unsigned char* state) {
 	tmp_state[14] = state[12] ^ state[13] ^ mul02[state[14]] ^ mul03[state[15]];
 	tmp_state[15] = mul03[state[12]] ^ state[13] ^ state[14] ^ mul02[state[15]];
 	for (unsigned short i = 0; i < stt_lng; i++) {
+#pragma HLS unroll
 		state[i] = tmp_state[i];
 	}
 }
@@ -101,45 +165,31 @@ void MixColumns(unsigned char* state) {
 void InvMixColumns(unsigned char* state) {
 #pragma HLS inline off
 	unsigned char tmp_state[stt_lng];
-	tmp_state[0] = mul14[state[0]] ^ mul11[state[1]] ^ mul13[state[2]]
-			^ mul09[state[3]];
-	tmp_state[1] = mul09[state[0]] ^ mul14[state[1]] ^ mul11[state[2]]
-			^ mul13[state[3]];
-	tmp_state[2] = mul13[state[0]] ^ mul09[state[1]] ^ mul14[state[2]]
-			^ mul11[state[3]];
-	tmp_state[3] = mul11[state[0]] ^ mul13[state[1]] ^ mul09[state[2]]
-			^ mul14[state[3]];
+	tmp_state[0] = mul14[state[0]] ^ mul11[state[1]] ^ mul13[state[2]] ^ mul09[state[3]];
+	tmp_state[1] = mul09[state[0]] ^ mul14[state[1]] ^ mul11[state[2]] ^ mul13[state[3]];
+	tmp_state[2] = mul13[state[0]] ^ mul09[state[1]] ^ mul14[state[2]] ^ mul11[state[3]];
+	tmp_state[3] = mul11[state[0]] ^ mul13[state[1]] ^ mul09[state[2]] ^ mul14[state[3]];
 
-	tmp_state[4] = mul14[state[4]] ^ mul11[state[5]] ^ mul13[state[6]]
-			^ mul09[state[7]];
-	tmp_state[5] = mul09[state[4]] ^ mul14[state[5]] ^ mul11[state[6]]
-			^ mul13[state[7]];
-	tmp_state[6] = mul13[state[4]] ^ mul09[state[5]] ^ mul14[state[6]]
-			^ mul11[state[7]];
-	tmp_state[7] = mul11[state[4]] ^ mul13[state[5]] ^ mul09[state[6]]
-			^ mul14[state[7]];
+	tmp_state[4] = mul14[state[4]] ^ mul11[state[5]] ^ mul13[state[6]] ^ mul09[state[7]];
+	tmp_state[5] = mul09[state[4]] ^ mul14[state[5]] ^ mul11[state[6]] ^ mul13[state[7]];
+	tmp_state[6] = mul13[state[4]] ^ mul09[state[5]] ^ mul14[state[6]] ^ mul11[state[7]];
+	tmp_state[7] = mul11[state[4]] ^ mul13[state[5]] ^ mul09[state[6]] ^ mul14[state[7]];
 
-	tmp_state[8] = mul14[state[8]] ^ mul11[state[9]] ^ mul13[state[10]]
-			^ mul09[state[11]];
-	tmp_state[9] = mul09[state[8]] ^ mul14[state[9]] ^ mul11[state[10]]
-			^ mul13[state[11]];
-	tmp_state[10] = mul13[state[8]] ^ mul09[state[9]] ^ mul14[state[10]]
-			^ mul11[state[11]];
-	tmp_state[11] = mul11[state[8]] ^ mul13[state[9]] ^ mul09[state[10]]
-			^ mul14[state[11]];
+	tmp_state[8] = mul14[state[8]] ^ mul11[state[9]] ^ mul13[state[10]] ^ mul09[state[11]];
+	tmp_state[9] = mul09[state[8]] ^ mul14[state[9]] ^ mul11[state[10]] ^ mul13[state[11]];
+	tmp_state[10] = mul13[state[8]] ^ mul09[state[9]] ^ mul14[state[10]] ^ mul11[state[11]];
+	tmp_state[11] = mul11[state[8]] ^ mul13[state[9]] ^ mul09[state[10]] ^ mul14[state[11]];
 
-	tmp_state[12] = mul14[state[12]] ^ mul11[state[13]] ^ mul13[state[14]]
-			^ mul09[state[15]];
-	tmp_state[13] = mul09[state[12]] ^ mul14[state[13]] ^ mul11[state[14]]
-			^ mul13[state[15]];
-	tmp_state[14] = mul13[state[12]] ^ mul09[state[13]] ^ mul14[state[14]]
-			^ mul11[state[15]];
-	tmp_state[15] = mul11[state[12]] ^ mul13[state[13]] ^ mul09[state[14]]
-			^ mul14[state[15]];
+	tmp_state[12] = mul14[state[12]] ^ mul11[state[13]] ^ mul13[state[14]] ^ mul09[state[15]];
+	tmp_state[13] = mul09[state[12]] ^ mul14[state[13]] ^ mul11[state[14]] ^ mul13[state[15]];
+	tmp_state[14] = mul13[state[12]] ^ mul09[state[13]] ^ mul14[state[14]] ^ mul11[state[15]];
+	tmp_state[15] = mul11[state[12]] ^ mul13[state[13]] ^ mul09[state[14]] ^ mul14[state[15]];
 	for (unsigned short i = 0; i < stt_lng; i++) {
+#pragma HLS unroll
 		state[i] = tmp_state[i];
 	}
 }
+
 
 void AddRoundKey(unsigned char* state, unsigned char* roundKey) {
 #pragma HLS inline off
@@ -148,6 +198,7 @@ void AddRoundKey(unsigned char* state, unsigned char* roundKey) {
 		state[i] ^= roundKey[i];
 	}
 }
+
 
 // Cipher
 void AES_Encrypt(unsigned char plaintext[stt_lng],
@@ -165,32 +216,45 @@ void AES_Encrypt(unsigned char plaintext[stt_lng],
 #pragma HLS array_map variable=mul02 instance=cipher horizontal
 #pragma HLS array_map variable=mul03 instance=cipher horizontal
 
-// Copy plaintext into state
+	// Copy plaintext into state
 	unsigned char state[stt_lng];
-	L_copy: for (unsigned short i = 0; i < stt_lng; i++) {
-//#pragma HLS pipeline
+	L_copy_i: for (unsigned short i = 0; i < stt_lng; i++) {
+		#pragma HLS unroll
 		state[i] = plaintext[i];
 	}
 
 	// Whitening
-	AddRoundKey(state, expandedKey + (stt_lng * 0)); // Round Key
+	unsigned char roundKey[stt_lng];
+	L_copy_rk0: for (unsigned short i = 0; i < stt_lng; i++) {
+		#pragma HLS unroll
+		roundKey[i] = expandedKey[stt_lng * 0 + i];
+	}
+	AddRoundKey(state, roundKey); // Round Key
 
-	L_rounds: for (unsigned short i = 0; i < Nr; i++) {
-//#pragma HLS pipeline
+	L_rounds: for (unsigned short j = 0; j < Nr; j++) {
+
 		SubBytes(state);
+
 		ShiftRows(state);
-		if (i != (Nr - 1)) {
+
+		if (j != (Nr - 1)) {
 			MixColumns(state);
 		}
-		AddRoundKey(state, expandedKey + (stt_lng * (i + 1))); // Round Key
+
+		L_copy_rk: for (unsigned short i = 0; i < stt_lng; i++) {
+			#pragma HLS unroll
+			roundKey[i] = expandedKey[stt_lng * (j + 1) + i];
+		}
+		AddRoundKey(state, roundKey); // Round Key
 	}
 
 	// Copy state to ciphertext
 	L_copy_o: for (unsigned short i = 0; i < stt_lng; i++) {
-//#pragma HLS unroll
+		#pragma HLS unroll
 		ciphertext[i] = state[i];
 	}
 }
+
 
 // Inverse Cipher
 void AES_Decrypt(unsigned char ciphertext[stt_lng],
@@ -210,46 +274,59 @@ void AES_Decrypt(unsigned char ciphertext[stt_lng],
 #pragma HLS array_map variable=mul13 instance=inverse_cipher horizontal
 #pragma HLS array_map variable=mul14 instance=inverse_cipher horizontal
 
-	// copy ciphertext into state
+	// Copy ciphertext into state
 	unsigned char state[stt_lng];
-	L_copy: for (unsigned short i = 0; i < stt_lng; i++) {
-//#pragma HLS pipeline
+	L_copy_i: for (unsigned short i = 0; i < stt_lng; i++) {
+		#pragma HLS unroll
 		state[i] = ciphertext[i];
 	}
 
-	AddRoundKey(state, expandedKey + (stt_lng * Nr));  // Round Key
+	unsigned char roundKey[stt_lng];
+	L_copy_rk0: for (unsigned short i = 0; i < stt_lng; i++) {
+		#pragma HLS unroll
+		roundKey[i] = expandedKey[stt_lng * Nr + i];
+	}
+	AddRoundKey(state, roundKey); // Round Key
 
-	L_rounds: for (unsigned short i = 0; i < Nr; i++) {
-//#pragma HLS pipeline
+	L_rounds: for (unsigned short j = 0; j < Nr; j++) {
+
 		InvShiftRows(state);
+
 		InvSubBytes(state);
-		AddRoundKey(state, expandedKey + (stt_lng * (Nr - i - 1))); // Round Key
-		if (i != (Nr - 1)) {
+
+		L_copy_rk: for (unsigned short i = 0; i < stt_lng; i++) {
+			#pragma HLS unroll
+			roundKey[i] = expandedKey[stt_lng * (Nr - j - 1) + i];
+		}
+		AddRoundKey(state, roundKey); // Round Key
+
+		if (j != (Nr - 1)) {
 			InvMixColumns(state);
 		}
 	}
 
 	// Copy state to plaintext
 	L_copy_o: for (unsigned short i = 0; i < stt_lng; i++) {
-//#pragma HLS unroll
+		#pragma HLS unroll
 		plaintext[i] = state[i];
 	}
 }
+
 
 // AES Full
 void AES_Full(bool mode_cipher, bool mode_inverse_cipher,
 		unsigned char data_in[stt_lng],
 		unsigned char expandedKey[ExtdCipherKeyLenghth_max], unsigned short Nr,
 		unsigned char data_out[stt_lng]) {
-#pragma HLS INTERFACE s_axilite port=mode_cipher bundle=CRTLS
-#pragma HLS INTERFACE s_axilite port=mode_inverse_cipher bundle=CRTLS
-#pragma HLS INTERFACE s_axilite port=data_in bundle=CRTLS
-#pragma HLS INTERFACE s_axilite port=expandedKey bundle=CRTLS
-#pragma HLS INTERFACE s_axilite port=Nr bundle=CRTLS
-#pragma HLS INTERFACE s_axilite port=data_out bundle=CRTLS
-#pragma HLS INTERFACe s_axilite port=return bundle=CRTLS
+		#pragma HLS INTERFACE s_axilite port=mode_cipher bundle=CRTLS
+		#pragma HLS INTERFACE s_axilite port=mode_inverse_cipher bundle=CRTLS
+		#pragma HLS INTERFACE s_axilite port=data_in bundle=CRTLS
+		#pragma HLS INTERFACE s_axilite port=expandedKey bundle=CRTLS
+		#pragma HLS INTERFACE s_axilite port=Nr bundle=CRTLS
+		#pragma HLS INTERFACE s_axilite port=data_out bundle=CRTLS
+		#pragma HLS INTERFACe s_axilite port=return bundle=CRTLS
 
-#pragma HLS inline region // will inline the functions unless inlining is off
+	#pragma HLS inline region // will inline the functions unless inlining is off
 	if (mode_cipher) {
 		AES_Encrypt(data_in, expandedKey, Nr, data_out);
 	}
--- VivadoHLS/source/test_AES_full.cpp
+++ VivadoHLS/source/test_AES_full.cpp
@@ -6,155 +6,70 @@ void AES_Full(bool mode_cipher, bool mode_inverse_cipher,
 		unsigned char expandedKey[ExtdCipherKeyLenghth_max], unsigned short Nr,
 		unsigned char data_out[stt_lng]);
 
-// Rijndael key schedule
-// https://en.wikipedia.org/wiki/Rijndael_key_schedule
-const unsigned char rcon[256] = { 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20,
-		0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc,
-		0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91,
-		0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33,
-		0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04,
-		0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a,
-		0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa,
-		0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25,
-		0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d,
-		0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8,
-		0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4,
-		0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61,
-		0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74,
-		0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b,
-		0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97,
-		0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39, 0x72, 0xe4,
-		0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83,
-		0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20,
-		0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc,
-		0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91,
-		0x39, 0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33,
-		0x66, 0xcc, 0x83, 0x1d, 0x3a, 0x74, 0xe8, 0xcb, 0x8d };
+void KeyExpansion(unsigned char* inputKey, unsigned short Nk, unsigned char* expandedKey);
 
-void KeyExpansionCore(unsigned char* in4, unsigned char i) {
-	// RotWord rotates left
-	// SubWord substitutes with S - Box value
-	unsigned char t = in4[0];
-	in4[0] = s_box[in4[1]];
-	in4[1] = s_box[in4[2]];
-	in4[2] = s_box[in4[3]];
-	in4[3] = s_box[t];
-	// RCon (round constant) 1st byte XOR rcon
-	in4[0] = in4[0] ^ rcon[i];
-}
 
-void SubWord(unsigned char* in4) {
-	// SubWord substitutes with S - Box value
-	in4[0] = s_box[in4[0]];
-	in4[1] = s_box[in4[1]];
-	in4[2] = s_box[in4[2]];
-	in4[3] = s_box[in4[3]];
-}
-
-void KeyExpansion(unsigned char* inputKey, unsigned short Nk,
-		unsigned char* expandedKey) {
-	unsigned short Nr = (Nk > Nb) ? Nk + 6 : Nb + 6; // = 10, 12 or 14 rounds
-	// Copy the inputKey at the beginning of expandedKey
-	for (unsigned short i = 0; i < Nk * rows; i++) {
-		expandedKey[i] = inputKey[i];
-	}
-
-	// Variables
-	unsigned short byGen = Nk * rows;
-	unsigned short rconIdx = 1;
-	unsigned char temp[rows];
-
-	// Generate expanded keys
-	while (byGen < (Nr + 1) * stt_lng) {
-		// Read previously generated last 4 bytes (last word)
-		for (unsigned short i = 0; i < rows; i++) {
-			temp[i] = expandedKey[byGen - rows + i];
-		}
-		// Perform KeyExpansionCore once for each 16 byte key
-		if (byGen % (Nk * rows) == 0) {
-			KeyExpansionCore(temp, rconIdx);
-			rconIdx++;
-		} else if ((Nk > 6) && (byGen % (Nk * rows) == (4 * rows))) {
-			SubWord(temp);
-		}
-		// XOR temp with [bytesGenerated-16] and store in expandedKeys
-		for (unsigned short i = 0; i < rows; i++) {
-			expandedKey[byGen] = expandedKey[byGen - Nk * rows] ^ temp[i];
-			byGen++;
-		}
-	}
-}
-
-int main() {
+int main()
+{
 	// These variables allow to change the AES length
 	// Nk = 4, 6 or 8 for AES 128, 192 or 256 respectively
 	// Only set Nk and the rest will compute according to the AES specification
-	unsigned short Nk = 4; // 4 or 6 or 8 [32-bit words] columns in cipher key
+	unsigned short Nk = 8; // 4 or 6 or 8 [32-bit words] columns in cipher key
 	unsigned short CipherKeyLenghth = Nk * rows; // bytes
 	unsigned short Nr = (Nk > Nb) ? Nk + 6 : Nb + 6; // = 10, 12 or 14 rounds
 	unsigned short ExtdCipherKeyLenghth = (Nr + 1) * stt_lng; // bytes in the expanded cipher key
 
-	cout << "AES with Nb = " << Nb << " columns, Nk = " << Nk
-			<< " (32-bit) words i.e. CipherKeyLenghth = " << CipherKeyLenghth
-			<< " bytes (or " << CipherKeyLenghth * 8 << " bits), Nr = " << Nr
-			<< " rounds" << endl << endl;
+    cout << "AES with Nb = " << Nb << " columns, Nk = " << Nk << " (32-bit) words i.e. CipherKeyLenghth = "
+        << CipherKeyLenghth << " bytes (or " << CipherKeyLenghth * 8 << " bits), Nr = " << Nr << " rounds" << endl << endl;
+
 
 	// create a dummy test cipher key
 	unsigned char key[CipherKeyLenghth_max];
 	cout << "key = ";
 	for (unsigned short i = 0; i < CipherKeyLenghth; i++) {
-		key[i] = (unsigned char) i;
-		printf("0x%X ", key[i]);
-	}
-	cout << endl << endl;
+		key[i] = (unsigned char)i;
+		cout << "0x" << hex << (unsigned short)key[i] << " ";
+	} cout << endl << endl;
 
-	// extend key
-	unsigned char expandedKey[ExtdCipherKeyLenghth_max];
-	KeyExpansion(key, Nk, expandedKey);
-	cout << "expandedKey = ";
-	for (unsigned short i = 0; i < ExtdCipherKeyLenghth; i++) {
-		printf("0x%X ", expandedKey[i]);
-	}
-	cout << endl << endl;
+
+    // extend key
+    unsigned char expandedKey[ExtdCipherKeyLenghth_max];
+    KeyExpansion(key, Nk, expandedKey);
+    cout << "expandedKey = ";
+    for (unsigned short i = 0; i < ExtdCipherKeyLenghth; i++) { cout << dec << (unsigned short)expandedKey[i] << " "; }
+    cout << "<=> ";
+    for (unsigned short i = 0; i < ExtdCipherKeyLenghth; i++) { cout << "0x" << hex << (unsigned short)expandedKey[i] << " "; }
+    cout << endl << endl;
+
 
 	// create a test input data (plaintext) (ABCDEFGHIJKLMNOP)
-	unsigned char plaintext[stt_lng] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-			'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P' };
+	unsigned char plaintext[stt_lng] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P' };
 	cout << "plaintext = ";
-	for (unsigned short i = 0; i < stt_lng; i++) {
-		printf("%c ", plaintext[i]);
-	}
-	cout << " <=> ";
-	for (unsigned short i = 0; i < stt_lng; i++) {
-		printf("%X ", plaintext[i]);
-	}
-	cout << endl << endl;
+    for (unsigned short i = 0; i < stt_lng; i++) { cout << plaintext[i] << " "; }
+    cout << "<=> ";
+    for (unsigned short i = 0; i < stt_lng; i++) { cout << "0x" << hex << (unsigned short)plaintext[i] << " "; }
+    cout << endl << endl;
+
 
 	// encrypt
 	unsigned char ciphertext[stt_lng];
 	AES_Full(true, false, plaintext, expandedKey, Nr, ciphertext);
 	cout << "ciphertext = ";
-	for (unsigned short i = 0; i < stt_lng; i++) {
-		printf("%X ", ciphertext[i]);
-	}
-	cout << " <=> ";
-	for (unsigned short i = 0; i < stt_lng; i++) {
-		printf("%d ", ciphertext[i]);
-	}
-	cout << endl;
+    for (unsigned short i = 0; i < stt_lng; i++) { cout << dec << (unsigned short)ciphertext[i] << " "; }
+    cout << "<=> ";
+    for (unsigned short i = 0; i < stt_lng; i++) { cout << "0x" << hex << (unsigned short)ciphertext[i] << " "; }
+    cout << endl << endl;
+
 
 	// decrypt
 	unsigned char decrypted_plaintext[stt_lng];
 	AES_Full(false, true, ciphertext, expandedKey, Nr, decrypted_plaintext);
 	cout << "decrypted_plaintext = ";
-	for (unsigned short i = 0; i < stt_lng; i++) {
-		printf("%c ", decrypted_plaintext[i]);
-	}
-	cout << " <=> ";
-	for (unsigned short i = 0; i < stt_lng; i++) {
-		printf("%X ", decrypted_plaintext[i]);
-	}
-	cout << endl << endl;
+    for (unsigned short i = 0; i < stt_lng; i++) { cout << decrypted_plaintext[i] << " "; }
+    cout << "<=> ";
+    for (unsigned short i = 0; i < stt_lng; i++) { cout << "0x" << hex << (unsigned short)decrypted_plaintext[i] << " "; }
+    cout << endl << endl;
+	
 
 	return 0;
 }
