6f6455b5ae3901a6f96a0128870b16735c29573e
Sergiu Mosanu <sm7ed@virginia.edu>
2019-02-15 12:28:10 -0500

    also add AES_Decrypt

---
+++ VivadoHLS/AES_Decrypt/solution1/script.tcl
@@ -0,0 +1,19 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2018 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project AES_Decrypt
+set_top AES_Decrypt
+add_files source/AESfunctions.cpp
+add_files source/AESfunctions.h
+add_files source/AEStables.h
+add_files -tb source/test_AES.cpp
+open_solution "solution1"
+set_part {xc7z020clg400-1} -tool vivado
+create_clock -period 10 -name default
+#source "./AES_Decrypt/solution1/directives.tcl"
+csim_design -clean
+csynth_design
+cosim_design
+export_design -rtl verilog -format ip_catalog
