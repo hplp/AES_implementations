7693a780403cf66bfd3fb008e7f9def0f9a5b82c
Sergiu Mosanu <sm7ed@virginia.edu>
2019-04-27 22:25:09 -0400

    no changes

--- VivadoHLS/AES_Full/solution1/script.tcl
+++ VivadoHLS/AES_Full/solution1/script.tcl
@@ -5,15 +5,18 @@
 ############################################################
 open_project AES_Full
 set_top AES_Full
-add_files source/AESfunctions.cpp
-add_files source/AESfunctions.h
-add_files source/AESkeys.h
 add_files source/AEStables.h
+add_files source/AESkeys.h
+add_files source/AESfunctions.h
+add_files source/AESfunctions.cpp
 add_files -tb source/test_AES_full.cpp -cflags "-Wno-unknown-pragmas"
 open_solution "solution1"
-set_part {xc7z020clg400-1} -tool vivado
+set_part {xc7z020clg400-1}
 create_clock -period 10 -name default
-config_export -description {AES Full HLS rolled} -display_name AES_Full_HLS_rolled -format ip_catalog -library HLS -rtl verilog -vendor UVA_HPLP
+config_compile -no_signed_zeros=0 -unsafe_math_optimizations=0
+config_export -description {AES Full HLS AXI4 Lite} -display_name AES_Full_HLS_AXI4Lite -format ip_catalog -library HLS -rtl verilog -vendor UVA_HPLP -vivado_phys_opt place -vivado_report_level 0
+config_sdx -optimization_level none -target none
+config_schedule -effort medium -relax_ii_for_timing=0
 #source "./AES_Full/solution1/directives.tcl"
 csim_design -clean
 csynth_design
--- VivadoHLS/AES_Full_axis8/solution1/script.tcl
+++ VivadoHLS/AES_Full_axis8/solution1/script.tcl
@@ -5,15 +5,16 @@
 ############################################################
 open_project AES_Full_axis8
 set_top AES_Full_axis8
-add_files source/AESfunctions.cpp
-add_files source/AESfunctions.h
-add_files source/AESkeys.h
 add_files source/AEStables.h
+add_files source/AESkeys.h
+add_files source/AESfunctions.h
+add_files source/AESfunctions.cpp
 add_files -tb source/test_AES_full_axis8.cpp -cflags "-Wno-unknown-pragmas"
 open_solution "solution1"
-set_part {xc7z020clg400-1} -tool vivado
+set_part {xc7z020clg400-1}
 create_clock -period 10 -name default
-config_export -description {AES Full with 8-bit AXI4 Stream} -display_name AES_Full_axis8 -format ip_catalog -library HLS -rtl verilog -vendor UVA_HPLP
+config_compile -no_signed_zeros=0 -unsafe_math_optimizations=0
+config_export -description {AES Full with 8-bit AXI4 Stream} -display_name AES_Full_axis8 -format ip_catalog -library HLS -rtl verilog -vendor UVA_HPLP -vivado_phys_opt place -vivado_report_level 0
 #source "./AES_Full_axis8/solution1/directives.tcl"
 csim_design -clean
 csynth_design
